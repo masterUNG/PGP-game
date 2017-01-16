67cccb2bbe19182003ee509692095162766f4b96
komkrid <game4670@gmail.com>
2017-01-16 17:53:10 +0700

    stater 160159

--- app/src/main/java/anuson/komkid/permitgeographypro/Register_farmer.java
+++ app/src/main/java/anuson/komkid/permitgeographypro/Register_farmer.java
@@ -2,8 +2,15 @@ package anuson.komkid.permitgeographypro;
 
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class Register_farmer extends Activity {
 
 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_farmer);
+
+
+    }
 }
